428e266ef7b0d277b6a8d71bd80b7c27f3cd9212
balena-ci <balena-ci@users.noreply.github.com>
2021-09-11 05:29:39 +0000

    deploy: 9320d9ca45882ceaab34739f9abdd55074a4ee60

---
+++ assets/search.js
@@ -0,0 +1,1 @@
+window.searchData = {"kinds":{"32":"Variable"},"rows":[{"id":0,"kind":32,"name":"getLogger","url":"modules.html#getLogger","classes":"tsd-kind-variable"}],"index":{"version":"2.3.9","fields":["name","parent"],"fieldVectors":[["name/0",[0,2.877]],["parent/0",[]]],"invertedIndex":[["getlogger",{"_index":0,"name":{"0":{}},"parent":{}}]],"pipeline":[]}}
