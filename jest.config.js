ef0f92593ac3540b6b803c93fe8741d407ee6633
Josh Bowling <josh@balena.io>
2022-06-19 17:07:19 +0900

    Drop @balena/jellyfish-config
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
-const base = require('@balena/jellyfish-config/config/jest.config')
-
 // NOTE: Because the unit tests in this module rely on capturing stdout,
 // jest has to be run in "verbose" mode for the correct output to appear, otherwise
 // jest will swallow the log messages and the tests will fail
 module.exports = {
-	...base,
+	preset: "ts-jest",
+	testEnvironment: "node",
+	roots: [
+		"lib",
+		"test",
+	],
 	verbose: true
 };
